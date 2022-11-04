898e489c578a1d494325bcd6931cf0d01594df86
laopengyou <stf30111994@gmail.com>
2022-11-04 20:22:29 +0100

    convert to hex number

--- src/Main.java
+++ src/Main.java
@@ -265,9 +265,123 @@ public class Main {
 //        Expected Output
 //
 //        Product of two binary numbers: 110
+//        long binary1, binary2, multiply = 0;
+//        int digit, factor = 1;
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input the first binary number: ");
+//        binary1 = in.nextLong();
+//        System.out.print("Input the second binary number: ");
+//        binary2 = in.nextLong();
+//        while (binary2 != 0)
+//        {
+//            digit = (int)(binary2 % 10);
+//            if (digit == 1)
+//            {
+//                binary1 = binary1 * factor;
+//                multiply = binaryproduct((int) binary1, (int) multiply);
+//            }
+//            else
+//            {
+//                binary1 = binary1 * factor;
+//            }
+//            binary2 = binary2 / 10;
+//            factor = 10;
+//        }
+//        System.out.print("Product of two binary numbers: " + multiply+"\n");
+//    }
+//    static int binaryproduct(int binary1, int binary2)
+//    {
+//        int i = 0, remainder = 0;
+//        int[] sum = new int[20];
+//        int binary_prod_result = 0;
+//
+//        while (binary1 != 0 || binary2 != 0)
+//        {
+//            sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+//            remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
+//            binary1 = binary1 / 10;
+//            binary2 = binary2 / 10;
+//        }
+//        if (remainder != 0)
+//        {
+//            sum[i++] = remainder;
+//        }
+//        --i;
+//        while (i >= 0)
+//        {
+//            binary_prod_result = binary_prod_result * 10 + sum[i--];
+//        }
+//        return binary_prod_result;
 
+//        short a, b;
+//        a = 0b10;
+//        b = 0b11;
+//
+//        System.out.println("Product of two binary numbers: " + Integer.toBinaryString(a*b));
+        // --------------------------------------------
+//        19. Write a Java program to convert a decimal number to binary number.
+//        Input Data:
+//        Input a Decimal Number : 5
+//        Expected Output
+//
+//        Binary number is: 101
+//        int dec_num, quot, i=1, j;
+//        int bin_num[] = new int[100];
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Input a Decimal Number : ");
+//        dec_num = scan.nextInt();
+//
+//        quot = dec_num;
+//
+//        while(quot != 0)
+//        {
+//            bin_num[i++] = quot%2;
+//            quot = quot/2;
+//        }
+//
+//        System.out.print("Binary number is: ");
+//        for(j=i-1; j>0; j--)
+//        {
+//            System.out.print(bin_num[j]);
+//        }
+//        System.out.print("\n");
 
-
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Input a decimal number: ");
+//        int decimal = input.nextInt();
+//        System.out.println("Binary number is: " + Integer.toBinaryString(decimal));
+        // --------------------------------------------
+//        20. Write a Java program to convert a decimal number to hexadecimal number.
+//        Input Data:
+//        Input a decimal number: 15
+//        Expected Output
+//
+//        Hexadecimal number is : F
+//        int dec_num,rem;
+//        String hexdec_num = "";
+//        char[] hex = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Input a decimal number: ");
+//        dec_num = in.nextInt();
+//
+//        while (dec_num > 0){
+//            rem = dec_num % 16;
+//            System.out.println("rem: " + rem);
+//            hexdec_num = hex[rem] + hexdec_num;
+//            System.out.println("hexdec_num: " + hexdec_num);
+//            dec_num = dec_num/16;
+//            System.out.println("dec_num: " + dec_num);
+//        }
+//        System.out.println("Hexadecimal number is : " + hexdec_num + "\n");
+        // --------------------------------------------
+//        21. Write a Java program to convert a decimal number to octal number.
+//        Input Data:
+//        Input a Decimal Number: 15
+//        Expected Output
+//
+//        Octal number is: 17
 
 
 
